2b57cbcadce6d02288e6b974995d8d2d4bfb2889
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2024-04-21 16:37:49 -0400

    Initial Commit

---
+++ goMLB/ContentView.swift
@@ -0,0 +1,26 @@
+//   ContentView.swift
+//   goMLB
+//
+//   Created by: Grant Perry on 4/21/24 at 4:37 PM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
