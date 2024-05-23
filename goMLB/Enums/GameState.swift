a802c2b3c542ae548ed01b45c4eb1c4c253b8c4d
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2024-05-23 12:10:09 -0400

    Working v4.1 with scores in picker

---
+++ goMLB/Enums/GameState.swift
@@ -0,0 +1,10 @@
+//   GameState.swift
+//   goMLB
+//
+//   Created by: Grant Perry on 5/22/24 at 11:51 AM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import Foundation
